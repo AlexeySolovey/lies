309cc489ba034e5be9107741e91b938ce1d6f9c2
Alexey <Solovey_alexey3@mail.ru>
2016-07-13 16:57:53 +0300

    sliders

--- js/common.js
+++ js/common.js
@@ -1,5 +1,20 @@
 ;(function($){
 	$(document).ready(function() {
-		
+		//slider for whom section
+		$('.whomSlider').bxSlider({
+			auto: true,
+			autoHover: true,
+			controls: false,
+			pager: false,
+			slideWidth: 300,
+			minSlides: 1,
+			maxSlides: 3,
+			moveSlides: 1,
+			slideMargin: 10
+		});
+		$('.reviews_slider').bxSlider({
+			infiniteLoop: false,
+			hideControlOnEnd: true
+		});
 	});
 })(jQuery)
